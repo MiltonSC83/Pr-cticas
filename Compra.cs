2e9bff4ea05967c850d1b0eff5a387896bf32e20
MiltonSC83 <42243730+MiltonSC83@users.noreply.github.com>
2018-09-04 08:54:40 -0500

    Practica 4
    Sobrecarga de operadores

--- Compra.cs
+++ Compra.cs
@@ -4,26 +4,46 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Practica2
+namespace SobrecargaDeOperadores
 {
-    public class Compra : Clientes
+    class Compra
     {
-        private string _MarcaDeCalzado;
-
-        public string MarcaDeCalzado { get => _MarcaDeCalzado; set => _MarcaDeCalzado = value; }
-
-        private double _NumeroCalzado;
-
-        public double NumeroCalzado { get => _NumeroCalzado; set => _NumeroCalzado = value; }
-
-        private string _ColorCalzado;
-
-        public string ColorCalzado { get => _ColorCalzado; set => _ColorCalzado = value; }
-
-        private int _PrecioCalzado;
-
-        public int PrecioCalzado { get => _PrecioCalzado; set => _PrecioCalzado = value; }
+        //Programa que realiza la compra de dos articulos diferentes, aplicando el 10% de descuento a uno de ellos
+        private double Articulo1;
+        private double Articulo2;
+        private double Total;
 
 
+       
+        public Compra(double Art1, double Art2 )
+        {
+            Articulo1 = Art1;
+            Articulo2 = Art2;
+
+            Total = Articulo1 + Articulo2;
+        }
+
+        public double Articulo11 { get => Articulo1; set => Articulo1 = value; }
+        public double Articulo21 { get => Articulo2; set => Articulo2 = value; }
+        public double Total1 { get => Total; set => Total = value; }
+
+        public override string ToString()
+        {
+            return string.Format("{0} + {1} ={2}", Articulo1, Articulo2, Total);
+        }
+
+        public static Compra operator +(Compra A1, Compra A2)
+        {
+            double sum = 0;
+            double sumDesc = 0;
+
+            sum = A1.Articulo1 * (1);
+            sumDesc = A2.Articulo2 * (.90);
+
+            Compra temp = new Compra(sum, sumDesc);
+            return temp;
+
+
+        }
     }
 }
---
+++ SobrecargaDeOperadores.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SobrecargaDeOperadores
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Compra C1 = new Compra(1,0);
+            Compra C2 = new Compra(0, 3);
+            Compra C3;
+
+            C3 = C1 + C2;
+
+            Console.WriteLine("El total de la compra es de: {0}", C3);
+            Console.ReadKey();
+        }
+    }
+}
