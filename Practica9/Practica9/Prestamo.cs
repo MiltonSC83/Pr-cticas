a4423d62d6c60cebd7696fe5ff99aedab1c686a5
MiltonSC83 <42243730+MiltonSC83@users.noreply.github.com>
2018-09-25 08:29:13 -0500

    Practica 9
    Interfaces

---
+++ Practica9/Practica9/Prestamo.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practica9
+{
+    class Prestamo: IBiblioteca
+    {
+        private string nombre;
+        private string fecha;
+        private double numerodecuenta;
+        private string libros;
+        private double adeudos;
+        
+
+        public Prestamo()
+        {
+            nombre = "";
+            fecha = "";
+            libros = "";
+            numerodecuenta = 0.00;
+            adeudos = 0.0;
+        }
+
+        public Prestamo(string n, string f, double a, string l, double nc)
+        {
+            nombre = n;
+            fecha = f;
+            numerodecuenta = nc;
+            libros = l;
+            adeudos = a;
+            
+            
+        }
+
+        public void muestraPrestamo()
+        {
+            Console.WriteLine("Alumno: {0}", nombre);
+            Console.WriteLine("Fecha: {0}", fecha);
+            Console.WriteLine("Número de cuenta: {0}", adeudos);
+            Console.WriteLine("Libros: {0}", libros);
+            Console.WriteLine("Debe: $ {0} ", getnumerodecuenta());
+            
+        }
+        public double getnumerodecuenta()
+        {
+            return numerodecuenta;
+        }
+    }
+}
---
+++ Practica9/Practica9/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practica9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Prestamo p1 = new Prestamo("Milton Osmar Sosa Corona", "25/09/2018",314308067, "El gesticulador", 0.00);
+            Prestamo p2 = new Prestamo("José Esteban Flores Toriz", "24/09/2018", 314265789, "El seductor de la patria", 12.50);
+
+            p1.muestraPrestamo();
+            p2.muestraPrestamo();
+            Console.ReadLine();
+        }
+    }
+}
